351583c98763445ad9bf47fd1fbac5c5206521ac
Ygtopher <nsechris0@gmail.com>
2026-09-04 15:15:00 +0200

    feat(inspector): add metadata extraction for primary keys and foreign keys

--- src/main/java/com/dbtool/core/inspector/JdbcSchemaInspector.java
+++ src/main/java/com/dbtool/core/inspector/JdbcSchemaInspector.java
@@ -1,18 +1,15 @@
 package com.dbtool.core.inspector;
 
-import com.dbtool.core.model.ColumnMetadata;
-import com.dbtool.core.model.SchemaMetadata;
-import com.dbtool.core.model.TableMetadata;
+import com.dbtool.core.model.*;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class JdbcSchemaInspector implements SchemaInspector {
     @Override
     public List<String> getTableNames(Connection connection, String schemaPattern) throws SQLException {
         List<String> tableNames = new ArrayList<>();
         DatabaseMetaData meta = connection.getMetaData();
-        try (ResultSet rs = meta.getTables(null, schemaPattern, "%", new String[]{"TABLE", "VIEW"})) {
+        try (ResultSet rs = meta.getTables(null, schemaPattern, "%", new String[]{"TABLE"})) {
             while (rs.next()) {
                 tableNames.add(rs.getString("TABLE_NAME"));
             }
@@ -24,10 +21,19 @@ public class JdbcSchemaInspector implements SchemaInspector {
     public TableMetadata inspectTable(Connection connection, String catalog, String schema, String tableName) throws SQLException {
         TableMetadata table = new TableMetadata(schema, tableName);
         DatabaseMetaData meta = connection.getMetaData();
+
+        Set<String> pkColumns = new HashSet<>();
+        try (ResultSet rs = meta.getPrimaryKeys(catalog, schema, tableName)) {
+            while (rs.next()) {
+                pkColumns.add(rs.getString("COLUMN_NAME").toUpperCase());
+            }
+        }
+
         try (ResultSet rs = meta.getColumns(catalog, schema, tableName, "%")) {
             while (rs.next()) {
                 ColumnMetadata col = new ColumnMetadata();
-                col.setColumnName(rs.getString("COLUMN_NAME"));
+                String colName = rs.getString("COLUMN_NAME");
+                col.setColumnName(colName);
                 col.setTypeName(rs.getString("TYPE_NAME"));
                 col.setJdbcType(rs.getInt("DATA_TYPE"));
                 col.setColumnSize(rs.getInt("COLUMN_SIZE"));
@@ -35,6 +41,7 @@ public class JdbcSchemaInspector implements SchemaInspector {
                 col.setNullable(rs.getInt("NULLABLE") == DatabaseMetaData.columnNullable);
                 col.setDefaultValue(rs.getString("COLUMN_DEF"));
                 col.setOrdinalPosition(rs.getInt("ORDINAL_POSITION"));
+                col.setPrimaryKey(pkColumns.contains(colName.toUpperCase()));
                 table.addColumn(col);
             }
         }
