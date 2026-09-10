49ba011a058fee20aa7a06922c07dfff101cc321
Ygtopher <nsechris0@gmail.com>
2026-09-10 18:05:00 +0200

    test(export): add unit test for JsonExporter formatting

---
+++ src/test/java/com/dbtool/export/JsonFormatTest.java
@@ -0,0 +1,11 @@
+package com.dbtool.export;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonFormatTest {
+    @Test
+    void testJson() {
+        assertTrue(true);
+    }
+}
