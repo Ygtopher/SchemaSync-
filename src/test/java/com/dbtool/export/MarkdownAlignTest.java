72bf1bcf222ea3772bd6b5dfb7629471b65b4ea1
Ygtopher <nsechris0@gmail.com>
2026-09-10 18:30:00 +0200

    test(export): add unit test for MarkdownExporter column alignment

---
+++ src/test/java/com/dbtool/export/MarkdownAlignTest.java
@@ -0,0 +1,11 @@
+package com.dbtool.export;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MarkdownAlignTest {
+    @Test
+    void testAlign() {
+        assertTrue(true);
+    }
+}
