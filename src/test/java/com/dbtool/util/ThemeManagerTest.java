24cac61bf24ada058d82b3af2112728ebbba1e60
Ygtopher <nsechris0@gmail.com>
2026-09-11 15:35:00 +0200

    feat(util): add SQL keyword capitalization in SqlFormatter

---
+++ src/test/java/com/dbtool/util/ThemeManagerTest.java
@@ -0,0 +1,11 @@
+package com.dbtool.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ThemeManagerTest {
+    @Test
+    void testTheme() {
+        assertTrue(ThemeManager.isDarkMode());
+    }
+}
