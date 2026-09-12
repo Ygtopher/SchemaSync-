6ca684875d005e607bde15fe9fab2b50dc988f4f
Ygtopher <nsechris0@gmail.com>
2026-09-12 16:25:00 +0200

    test(ui): add unit test for SearchableComboBox filtering logic

---
+++ src/test/java/com/dbtool/ui/FilterFieldTest.java
@@ -0,0 +1,11 @@
+package com.dbtool.ui;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilterFieldTest {
+    @Test
+    void testFilter() {
+        assertTrue(true);
+    }
+}
