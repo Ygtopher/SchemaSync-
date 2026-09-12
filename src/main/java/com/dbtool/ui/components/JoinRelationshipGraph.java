5dfba17453269d7558de6f15593694a4a5f04ad0
Ygtopher <nsechris0@gmail.com>
2026-09-12 13:30:00 +0200

    feat(ui): configure SQL syntax highlighting themes for dark and light modes

---
+++ src/main/java/com/dbtool/ui/components/JoinRelationshipGraph.java
@@ -0,0 +1,7 @@
+package com.dbtool.ui.components;
+
+import java.io.Serializable;
+
+public class JoinRelationshipGraph implements Serializable {
+    private static final long serialVersionUID = 1L;
+}
