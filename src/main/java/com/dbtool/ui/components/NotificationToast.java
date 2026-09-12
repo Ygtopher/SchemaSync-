940693162a6e203884adef906cfa1168c4dfc157
Ygtopher <nsechris0@gmail.com>
2026-09-12 12:00:00 +0200

    feat(ui): implement SqlSyntaxHighlighter integrating RSyntaxTextArea

---
+++ src/main/java/com/dbtool/ui/components/NotificationToast.java
@@ -0,0 +1,9 @@
+package com.dbtool.ui.components;
+
+import javax.swing.JLabel;
+
+public class NotificationToast extends JLabel {
+    public NotificationToast(String msg) {
+        super(msg);
+    }
+}
