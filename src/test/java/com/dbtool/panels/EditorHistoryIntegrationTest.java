00241bf6526bbd7a0154869bcd828625c9f2a671
Ygtopher <nsechris0@gmail.com>
2026-09-13 19:50:00 +0200

    refactor(panels): link SqlEditorPanel to QueryHistory and SavedQueries

---
+++ src/test/java/com/dbtool/panels/EditorHistoryIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.dbtool.panels;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EditorHistoryIntegrationTest {
+    @Test
+    void testHistory() {
+        assertTrue(true);
+    }
+}
