52f59a80de781c5c209f1402b503e3b6c7625fe4
Ygtopher <nsechris0@gmail.com>
2026-09-13 20:30:00 +0200

    refactor(panels): add Copy SQL button to all query generation panels

---
+++ src/test/java/com/dbtool/panels/CopySqlButtonsTest.java
@@ -0,0 +1,11 @@
+package com.dbtool.panels;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CopySqlButtonsTest {
+    @Test
+    void testCopy() {
+        assertTrue(true);
+    }
+}
