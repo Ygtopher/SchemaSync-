95b06cd7f00927bda9cde9c3cf3ebc49cb249e9d
Ygtopher <nsechris0@gmail.com>
2026-09-16 08:12:44 +0200

    style: Increase JSplitPane divider size and one-touch button size

--- src/main/java/com/dbtool/util/ThemeManager.java
+++ src/main/java/com/dbtool/util/ThemeManager.java
@@ -44,6 +44,11 @@ public class ThemeManager {
             UIManager.put("ScrollBar.thumbArc", 4);
             UIManager.put("ScrollBar.thumbInsets", new java.awt.Insets(2, 2, 2, 2));
             
+            UIManager.put("SplitPane.dividerSize", 12);
+            UIManager.put("SplitPane.centerOneTouchButtons", true);
+            UIManager.put("SplitPane.oneTouchButtonSize", 10);
+            UIManager.put("SplitPane.oneTouchButtonOffset", 2);
+            
             for (Window window : Window.getWindows()) {
                 SwingUtilities.updateComponentTreeUI(window);
             }
